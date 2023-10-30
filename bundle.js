15dc326f8ecc3bc367b786d2c42cf4dc98d8c77a
“emilesherrott” <emilesherrott@gmail.com>
2023-10-30 16:59:05 +0000

    pixabay

--- bundle.js
+++ bundle.js
@@ -3,24 +3,34 @@ const fruitForm = document.querySelector("#inputSection form")
 fruitForm.addEventListener("submit", extractFruit)
 
 function extractFruit(e) {
-    e.preventDefault()
-    fetchFruitData(e.target[0].value)
-    e.target[0].value = ""
+  e.preventDefault()
+  fetchFruitData(e.target[0].value)
+  e.target[0].value = ""
 }
 
-function fetchFruitData(fruit){
-    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
-        .then(processResponse)
-        .then(data => addFruit(data))
-        .catch(err => console.log(err))
-}
+// function fetchFruitData(fruit){
+//     fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+//         .then(processResponse)
+//         .then(data => addFruit(data))
+//         .catch(err => console.log(err))
+// }
 
-function processResponse(resp) {
-    if(resp.ok){
-        return resp.json()
+const fetchFruitData = async (fruit) => {
+  try {
+    const response = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+    const fruityResponse = await fetch(`https://pixabay.com/api/?q=${fruit}+fruit&key=35290745-f05685bdfcd0a1a9b3d3833b0`)
+    console.log(fruityResponse)
+    if (response.ok && fruityResponse.ok) {
+      const data = await response.json()
+      const { hits } = await fruityResponse.json()
+      console.log(hits)
+      addFruit(data, hits)
     } else {
-        throw "Error: http status code = " + resp.status
+      throw "Error: http status code = " + response.status
     }
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 const fruitList = document.querySelector("#fruitSection ul")
@@ -29,33 +39,35 @@ const fruitNutrition = document.querySelector("#nutritionSection p")
 let calories = 0
 const fruitCal = {}
 
-function addFruit(fruit) {
-    if(!fruit){
-        console.log("Invalid fruit")
-    } else {
+function addFruit(fruit, imageObj) {
+  if (!fruit || !imageObj) {
+    console.log("Invalid fruit")
+  } else {
     const li = document.createElement("li")
-    li.addEventListener("click", removeFruit, {once:true})
-    li.textContent = fruit['name']
+    const img = document.createElement("img")
+    li.addEventListener("click", removeFruit, { once: true })
+    li.textContent = fruit["name"]
+    img.setAttribute("src", imageObj[0].previewURL)
+    li.appendChild(img)
     fruitList.appendChild(li)
+
+
 
     fruitCal[fruit.name] = fruit.nutritions.calories
 
     calories += fruit.nutritions.calories
     fruitNutrition.textContent = calories
-    } 
+  }
 }
 
-function removeFruit(e){
-    const fruitName = e.target.textContent
-    calories -= fruitCal[fruitName]
-    fruitNutrition.textContent = calories
+function removeFruit(e) {
+  const fruitName = e.target.textContent
+  calories -= fruitCal[fruitName]
+  fruitNutrition.textContent = calories
 
-    delete fruitCal[fruitName]
+  delete fruitCal[fruitName]
 
-
-    e.target.remove()
+  e.target.remove()
 }
-
-
 
 },{}]},{},[1]);
--- script.js
+++ script.js
@@ -17,9 +17,13 @@ function extractFruit(e) {
 const fetchFruitData = async (fruit) => {
   try {
     const response = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
-    if (response.ok) {
+    const fruityResponse = await fetch(`https://pixabay.com/api/?q=${fruit}+fruit&key=35290745-f05685bdfcd0a1a9b3d3833b0`)
+    console.log(fruityResponse)
+    if (response.ok && fruityResponse.ok) {
       const data = await response.json()
-      addFruit(data)
+      const { hits } = await fruityResponse.json()
+      console.log(hits)
+      addFruit(data, hits)
     } else {
       throw "Error: http status code = " + response.status
     }
@@ -34,14 +38,19 @@ const fruitNutrition = document.querySelector("#nutritionSection p")
 let calories = 0
 const fruitCal = {}
 
-function addFruit(fruit) {
-  if (!fruit) {
+function addFruit(fruit, imageObj) {
+  if (!fruit || !imageObj) {
     console.log("Invalid fruit")
   } else {
     const li = document.createElement("li")
+    const img = document.createElement("img")
     li.addEventListener("click", removeFruit, { once: true })
     li.textContent = fruit["name"]
+    img.setAttribute("src", imageObj[0].previewURL)
+    li.appendChild(img)
     fruitList.appendChild(li)
+
+
 
     fruitCal[fruit.name] = fruit.nutritions.calories
 
