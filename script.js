0306ee01b9d24e99e876c756f962f7e4b4b43fed
“emilesherrott” <emilesherrott@gmail.com>
2023-10-30 16:43:02 +0000

    async/await

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ const fruitForm = document.querySelector("#inputSection form")
 fruitForm.addEventListener("submit", extractFruit)
 
 function extractFruit(e) {
-    e.preventDefault()
-    fetchFruitData(e.target[0].value)
-    e.target[0].value = ""
+  e.preventDefault()
+  fetchFruitData(e.target[0].value)
+  e.target[0].value = ""
 }
 
-function fetchFruitData(fruit){
-    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
-        .then(processResponse)
-        .then(data => addFruit(data))
-        .catch(err => console.log(err))
-}
+// function fetchFruitData(fruit){
+//     fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+//         .then(processResponse)
+//         .then(data => addFruit(data))
+//         .catch(err => console.log(err))
+// }
 
-function processResponse(resp) {
-    if(resp.ok){
-        return resp.json()
+const fetchFruitData = async (fruit) => {
+  try {
+    const response = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+    if (response.ok) {
+      const data = await response.json()
+      addFruit(data)
     } else {
-        throw "Error: http status code = " + resp.status
+      throw "Error: http status code = " + response.status
     }
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 const fruitList = document.querySelector("#fruitSection ul")
@@ -29,30 +35,27 @@ let calories = 0
 const fruitCal = {}
 
 function addFruit(fruit) {
-    if(!fruit){
-        console.log("Invalid fruit")
-    } else {
+  if (!fruit) {
+    console.log("Invalid fruit")
+  } else {
     const li = document.createElement("li")
-    li.addEventListener("click", removeFruit, {once:true})
-    li.textContent = fruit['name']
+    li.addEventListener("click", removeFruit, { once: true })
+    li.textContent = fruit["name"]
     fruitList.appendChild(li)
 
     fruitCal[fruit.name] = fruit.nutritions.calories
 
     calories += fruit.nutritions.calories
     fruitNutrition.textContent = calories
-    } 
+  }
 }
 
-function removeFruit(e){
-    const fruitName = e.target.textContent
-    calories -= fruitCal[fruitName]
-    fruitNutrition.textContent = calories
+function removeFruit(e) {
+  const fruitName = e.target.textContent
+  calories -= fruitCal[fruitName]
+  fruitNutrition.textContent = calories
 
-    delete fruitCal[fruitName]
+  delete fruitCal[fruitName]
 
-
-    e.target.remove()
+  e.target.remove()
 }
-
-
