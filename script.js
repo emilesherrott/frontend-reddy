8a84473b2d82c52c07607edffbb6be17a7bd4c20
“emilesherrott” <emilesherrott@gmail.com>
2023-10-30 15:22:26 +0000

    Latest code

--- script.js
+++ script.js
@@ -3,17 +3,49 @@ fruitForm.addEventListener("submit", extractFruit)
 
 function extractFruit(e) {
     e.preventDefault()
-    addFruit(e.target[0].value)
+    fetchFruitData(e.target[0].value)
     e.target[0].value = ""
 }
 
+function fetchFruitData(fruit){
+    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+        // .then(processResponse)
+        .then(resp => resp.json())
+        .then(data => addFruit(data))
+        .catch(err => console.log(err))
+}
+
+function processResponse(resp) {
+    console.log(resp, "hello")
+    if(resp.ok){
+        return resp.json()
+    } else {
+        throw "Error: http status code = " + resp.status
+    }
+}
+
 const fruitList = document.querySelector("#fruitSection ul")
+const fruitNutrition = document.querySelector("#nutritionSection p")
+
+let calories = 0
+
 function addFruit(fruit) {
+    console.log(fruit)
     if(!fruit){
         console.log("Invalid fruit")
     } else {
     const li = document.createElement("li")
-    li.textContent = fruit
+    li.addEventListener("click", removeFruit, {once:true})
+    li.textContent = fruit['name']
     fruitList.appendChild(li)
+
+    calories += fruit.nutritions.calories
+    fruitNutrition.textContent = calories
     } 
 }
+
+function removeFruit(e){
+    e.target.remove()
+}
+
+
