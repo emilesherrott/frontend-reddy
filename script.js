5921882b55f383ecf60dd2ffcc13fc44a7e74538
“emilesherrott” <emilesherrott@gmail.com>
2023-10-30 12:29:40 +0000

    Nearly finished code

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const fruitForm = document.querySelector("#inputSection form")
-console.log(fruitForm)
+fruitForm.addEventListener("submit", extractFruit)
 
+function extractFruit(e) {
+    e.preventDefault()
+    addFruit(e.target[0].value)
+    e.target[0].value = ""
+}
 
+const fruitList = document.querySelector("#fruitSection ul")
+function addFruit(fruit) {
+    if(!fruit){
+        console.log("Invalid fruit")
+    } else {
+    const li = document.createElement("li")
+    li.textContent = fruit
+    fruitList.appendChild(li)
+    } 
+}
